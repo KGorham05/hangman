73cc84e2b356ec30ce0bf243a8c871a262da6f1d
Kgor <kevin.gorham@gmail.com>
2019-02-22 17:54:52 -0800

    got correct letter guesses to replace dashes in answer array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,12 @@
 // Psuedo Code
 
 // Display some things on the page
-// game board
 
 // Create variables for game board
 // win tally
 var wins = 0;
-// blank spaces for each letter in the answer string
-var answerString = [];
-// reveal letters as the user guesses the correct letters
+// blank spaces for each letter in the solution string (reveal as guessed correctly)
+let blankAnswerArray = [];
 // Number of guesses remaining for the user
 var guesses = 10;
 // Letters already guessed displayed like L Z Y H.
@@ -17,45 +15,63 @@ var guessedLetters = [];
 // Create variables that hold references to the places
 // in the HTML where we want to display things.
 var winsText = document.getElementById("wins-text");
-var answerStringText = document.getElementById("blank-spaces")
+var answerArrayText = document.getElementById("blank-spaces")
 var guessesText = document.getElementById("remaining-guesses-text");
 var previousGuessesText = document.getElementById("previous-guesses-text");
 
 // Answer Array
-var answers = ["Optimus Prime", "The Terminator", "Mega Man", "R2-D2", "Number Six", "Bender", "Flexo", "Calculon", "WALL-E", "C-3PO", "The Iron Giant", "Robocop", "Hal 9000", "Data", "Johnny 5" ];
-// computer generates a starting answer from the robot list array.
-var answer = answers[Math.floor(Math.random() * answers.length)];
+var possibleAnswers = ["Optimus Prime", "The Terminator", "Mega Man", "R2-D2", "Number Six", "Bender", "Flexo", "Calculon", "WALL-E", "C-3PO", "The Iron Giant", "Robocop", "Hal 9000", "Data", "Johnny 5"];
+// computer generates a starting solution from the robot list array.
+var solution1 = possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)];
+var solution = solution1.toUpperCase();
 
-// Populate the answerString with blanks for each character in the answer string
-// I need to create spaces where there is a space in the string, and reveal the dash if there is a dash in the answer. 
-answer.forEach(function() {
-    console.log("sytax check in my forEach statement");
-})
-for (i = 0; i < answer.length; i++) {
-        // check if a character is a space
-        // check if a character is a dash
-        answerString.push("_");
-    console.log(answerString);
-}
+// Populate the blankAnswerArray with blanks for each character in the solution string
+// I need to create spaces where there is a space in the string, and reveal the dash if there is a dash in the solution.
+// Ryan B. Solution
+// solution.forEach(function(value, key) {
+//     if (value == "-" || value == " ") {
+//         solution[key]=value
+//     } else {solution[key]="_";}
+// }); 
 
-document.getElementById("blank-spaces").textContent = answerString;
+for (i = 0; i < solution.length; i++) {
+    if (solution.charAt(i) == " ") {
+        blankAnswerArray.push(" ");
+    } else if (solution.charAt(i) == "-") {
+        blankAnswerArray.push("-");
+    } else {
+        blankAnswerArray.push("_");
+    };
+};
+console.log(solution);
+
+// display the blank answer array
+answerArrayText.textContent = blankAnswerArray;
 
 // This will run whenever the user presses a key.
 document.onkeyup = function (event) {
     console.log("Game function is running");
-    
-    console.log(answer);
+    console.log(solution);
 
     // determine which key is pressed
     var userGuess = event.key;
 
- 
-    // compare that key to all index positions of the answer string
-
+    // compare that key to all index positions of the solution string
+    for (j = 0; j < solution.length; j++) {
+        if (solution[j] === userGuess.toUpperCase()) {
+            blankAnswerArray[j] = userGuess.toUpperCase();
+            // update the blankAnswerArray
+            answerArrayText.textContent = blankAnswerArray;
+        } else {
+            // reduce guesses remaining 
+            guesses--; 
+            
+        };
+    };
 
 
 };
-//After the user wins/loses the game should auto choose another word +overwrite answer string with blanks
+//After the user wins/loses the game should auto choose another word +overwrite solution string with blanks
 
 // BONUS see instructions
 
